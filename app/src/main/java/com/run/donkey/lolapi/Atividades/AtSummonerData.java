b9ef75233e9d625a2300b746e86cefb27448ea58
Nuno <nmgomesalves@gmail.com>
2015-10-30 00:26:18 +0000

    intro logo + icon added

--- app/src/main/java/com/run/donkey/lolapi/Atividades/AtSummonerData.java
+++ app/src/main/java/com/run/donkey/lolapi/Atividades/AtSummonerData.java
@@ -1,12 +1,17 @@
 package com.run.donkey.lolapi.Atividades;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.run.donkey.lolapi.Adapters.AdapterSummoner;
 import com.run.donkey.lolapi.ClassesAuxiliares.SummonerDto;
@@ -14,6 +19,7 @@ import com.run.donkey.lolapi.R;
 
 import java.util.ArrayList;
 import java.util.Map;
+
 
 public class AtSummonerData extends AppCompatActivity {// Atividade
 
@@ -29,10 +35,33 @@ public class AtSummonerData extends AppCompatActivity {// Atividade
     String nome;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_overflow, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId()){
+            case R.id.Themes:
+                Toast.makeText(getBaseContext(), "Change your theme", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.atsummonerdata);
+
         sum = new ArrayList<>();
         bit = new ArrayList<>();
 
@@ -47,13 +76,13 @@ public class AtSummonerData extends AppCompatActivity {// Atividade
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {// clickar no botão segue
                 nome = buscaSum.getText().toString();
 
                 SummonerDto summoner = new SummonerDto(adapterS, sum, mapa, bit);
                 summoner.getSummonerData(buscaSum.getText().toString());
 
-                //      new getSummonerTask().execute(buscaSum.getText().toString());// Inicia procura
+
 
             }
         });
